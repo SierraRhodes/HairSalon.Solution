ae4374f71dd48de6adb57079e0676053b929a20e
SierraRhodes <alreadywrote@gmail.com>
2023-05-22 16:57:32 -0700

    fix create client bug

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -24,7 +24,7 @@ namespace HairSalon.Controllers
 
     public ActionResult Create()
 {
-  ViewBag.Stylist.Id = new SelectList(_db.Stylists, "Stylist.Id", "Name");
+  ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
   return View();
 }
 
